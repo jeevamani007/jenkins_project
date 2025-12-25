5a401ac98e847fea30880517a3c1c0d50dde793d
jeevamani007 <ponjeevabsccs@gmail.com>
2025-12-25 22:17:40 +0530

    reuqeiments change

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Jeeva%40123@localhost:5432/detection"
+DATABASE_URL = "postgresql://detection_weh6_user:Jeeva%40123@dpg-d56mhmp5pdvs738fr5cg-a:5432/detection_weh6"
+
 
 # Create engine
 engine = create_engine(DATABASE_URL)
